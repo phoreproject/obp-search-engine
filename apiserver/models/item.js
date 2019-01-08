7c188fd6819e677df27ebb3ebb7846bc5990cf37
anchaj <anchaj@users.noreply.github.com>
2019-01-08 01:27:41 +0100

    add new fields to models

--- apiserver/models/item.js
+++ apiserver/models/item.js
@@ -7,20 +7,32 @@ module.exports = function(sequelize, DataTypes){
             unique: false,
             primaryKey: true
         },
+        score: DataTypes.INTEGER,
         slug: DataTypes.STRING(70),
         title: DataTypes.STRING(140),
         tags: DataTypes.STRING(410),
+        categories: DataTypes.STRING(410),
+        contractType: DataTypes.STRING(20),
         description: DataTypes.STRING(50000),
-        thumbnail: DataTypes.STRING(160),
+        thumbnail: DataTypes.STRING(260),
         language: DataTypes.STRING(20),
+
+        //price
         priceAmount: DataTypes.BIGINT,
         priceCurrency: DataTypes.STRING(10),
-        categories: DataTypes.STRING(410),
+        modifier: DataTypes.INTEGER,
+
         nsfw: DataTypes.BOOLEAN,
-        contractType: DataTypes.STRING(20),
-        rating: DataTypes.DECIMAL(3, 2)
+        averageRating: DataTypes.INTEGER,
+        ratingCount: DataTypes.INTEGER,
+
+        coinType: DataTypes.STRING(20),
+        coinDivisibility: DataTypes.INTEGER,
+        normalizedPrice: DataTypes.DOUBLE,
+
+        rating: DataTypes.DECIMAL(3, 2) // maybe it is average rating
     }, {
         freezeTableName: true,
         timestamps: false
-    })
+    });
 };
