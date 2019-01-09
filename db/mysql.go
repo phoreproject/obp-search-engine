45450c603aab754e1b5ed8c62f8269c5d398aae2
anchaj <anchaj@users.noreply.github.com>
2019-01-09 20:03:45 +0100

    add missing fields into nodes table

--- db/mysql.go
+++ db/mysql.go
@@ -19,10 +19,11 @@ type SQLDatastore struct {
 
 // NewSQLDatastore creates a new datastore given MySQL connection info
 func NewSQLDatastore(db *sql.DB) (*SQLDatastore, error) {
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS nodes (userAgent VARCHAR(50), id VARCHAR(50) NOT NULL, lastUpdated DATETIME, name VARCHAR(40), " +
-		"handle VARCHAR(40), location VARCHAR(40), nsfw TINYINT(1), vendor TINYINT(1), moderator TINYINT(1), about VARCHAR(10000), " +
-		"shortDescription VARCHAR(160), followerCount INT, followingCount INT, listingCount INT, postCount INT, ratingCount INT, " +
-		"averageRating DECIMAL(3, 2), listed TINYINT(1) DEFAULT 0, blocked TINYINT(1) DEFAULT 0, PRIMARY KEY (id))")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS nodes (userAgent VARCHAR(50), id VARCHAR(50) NOT NULL, lastUpdated DATETIME, " +
+		"name VARCHAR(40), handle VARCHAR(40), location VARCHAR(40), nsfw TINYINT(1), vendor TINYINT(1), moderator TINYINT(1), " +
+		"verifiedModerator TINYINT(1) DEFAULT 0, about VARCHAR(10000), shortDescription VARCHAR(160), followerCount INT, " +
+		"followingCount INT, listingCount INT, postCount INT, ratingCount INT, averageRating DECIMAL(3, 2), listed TINYINT(1) DEFAULT 0, " +
+		"blocked TINYINT(1) DEFAULT 0, PRIMARY KEY (id))")
 	if err != nil {
 		return nil, err
 	}
