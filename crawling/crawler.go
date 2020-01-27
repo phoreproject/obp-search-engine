00a1326c70282673b7b3d6b01af3cb5d2f13c1c1
anchaj <anchaj@users.noreply.github.com>
2020-01-27 20:54:53 +0100

    Move functionality from main to crawler class.

--- crawling/crawler.go
+++ crawling/crawler.go
@@ -1,17 +1,63 @@
 package crawling
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"net/http"
+	"path"
 	"strings"
+	"sync"
 	"time"
 )
 
+const (
+	BanThreshold   = 0.5
+	AllowThreshold = 0.1
+)
+
+func checkListings(url string, items []Item) ([]int, error) {
+	c := &http.Client{Timeout: time.Second * 10}
+
+	s, err := json.Marshal(items)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", "http://"+path.Join(url, "checkListings"), bytes.NewBuffer(s))
+	req.Header.Set("Content-type", "application/json")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	decoder := json.NewDecoder(resp.Body)
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(fmt.Sprintf("Server returned %s", resp.Status))
+	}
+
+	var response []int
+	err = decoder.Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
 // Crawler handles crawling through each node in OB
 type Crawler struct {
-	DB           Datastore
-	RPCInterface RPCInterface
-	WorkerQueue  chan chan Node
+	DB                Datastore
+	RPCInterface      RPCInterface
+	WorkerQueue       chan chan Node
+	MaxCoroutineCnt   int
+	ChunkSize         int
+	HttpClassifierUrl string
 }
 
 // CrawlNode runs the crawler for one step
@@ -57,4 +103,134 @@ func (c Crawler) CrawlNode(nextNodeID string) error {
 		}
 	}
 	return nil
+}
+
+func (c Crawler) classify(nodeID string, items []Item) {
+	log.Debugf("Checking listings status using external service")
+	output, err := checkListings(c.HttpClassifierUrl, items)
+	if err != nil {
+		log.Warning(err)
+	}
+	log.Debugf("Check finished with %s", output)
+
+	if len(output) == len(items) {
+		bannedCnt := 0
+		for index, blocked := range output {
+			if blocked != 0 {
+				bannedCnt += 1
+			}
+			items[index].Blocked = blocked > 0
+		}
+
+		if bannedCnt == 0 || float64(len(items))/float64(bannedCnt) < AllowThreshold { // automatically list owner
+			// automatically list stores with low level of banned listings
+			err := c.DB.UpdateNodeStatus(nodeID, "listed", true)
+			if err != nil {
+				log.Warning("Cannot update node status listed = true for node %s", nodeID)
+				log.Error(err)
+			} else {
+				log.Debugf("Updated node status listed = true for node %s", nodeID)
+			}
+		} else if float64(len(items))/float64(bannedCnt) > BanThreshold {
+			// automatically ban also entire owner store
+			err := c.DB.UpdateNodeStatus(nodeID, "blocked", true)
+			if err != nil {
+				log.Warning("Cannot update node status banned = true for node %s", nodeID)
+				log.Error(err)
+			} else {
+				log.Debugf("Updated node status banned = true for node %s", nodeID)
+			}
+		} else {
+			log.Debugf("Node %s banned listing threshold in range (%f < %f < %f) - cannot decide automatically",
+				nodeID, AllowThreshold, float64(len(items))/float64(bannedCnt), BanThreshold)
+		}
+	}
+}
+
+func (c Crawler) nodeLoop(nodeID string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Debugf("Processing node with id: %s\n", nodeID)
+	err := c.CrawlNode(nodeID)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	log.Debugf("Crawling items for nodeID: %s\n", nodeID)
+	items, err := c.RPCInterface.GetItems(nodeID)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	if c.HttpClassifierUrl != "" && len(items) > 0 {
+		c.classify(nodeID, items)
+	}
+
+	err = c.DB.AddItemsForNode(nodeID, items)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+}
+
+func (c Crawler) MainLoop() {
+	crawlerRound := 1
+	for {
+		startTime := time.Now()
+		processedCnt := 0
+		done := make(chan bool)
+		go func() {
+			lastNodeID := ""
+			for {
+				// get next chunk of nodes from database
+				nodesIDs, err := c.DB.GetNextNodesChan(lastNodeID, c.ChunkSize)
+				if err != nil {
+					done <- true
+					return
+				}
+
+				atLeastOneNode := false
+				for nodeID := range nodesIDs {
+					atLeastOneNode = true
+
+					// create list of at max MAX_PARALLEL_COROUTINE nodes to start parallel coroutines
+					var lastNodes []string
+					lastNodes = append(lastNodes, nodeID)
+					nodesLen := c.MaxCoroutineCnt - 1
+					for nodeID = range nodesIDs {
+						lastNodes = append(lastNodes, nodeID)
+						nodesLen--
+						if nodesLen <= 0 {
+							break
+						}
+					}
+					lastNodeID = nodeID
+
+					// start len(lastNodes) parallel coroutines
+					var wg sync.WaitGroup
+					wg.Add(len(lastNodes))
+					processedCnt += len(lastNodes)
+					processingStart := time.Now()
+					for i := range lastNodes {
+						go c.nodeLoop(lastNodes[i], &wg)
+					}
+					wg.Wait()
+					log.Debugf("Processing of %d nodes took %s.", len(lastNodes), time.Since(processingStart))
+				}
+
+				if !atLeastOneNode {
+					break
+				}
+			}
+			done <- true
+		}()
+
+		select {
+		case <-done:
+			crawlerRound++
+			log.Debugf("Crawler round %d took %s and processed %d items", crawlerRound, time.Since(startTime), processedCnt)
+			processedCnt = 0
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,185 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"database/sql"
-	"encoding/json"
 	"flag"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/phoreproject/obp-search-engine/crawling"
 	"github.com/phoreproject/obp-search-engine/db"
 	"github.com/phoreproject/obp-search-engine/rpc"
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"io"
-	"net/http"
 	"os"
-	"path"
-	"sync"
-	"time"
 )
-
-const (
-	BanThreshold   = 0.5
-	AllowThreshold = 0.1
-)
-
-func checkListings(url string, items []crawling.Item) ([]int, error) {
-	c := &http.Client{Timeout: time.Second * 10}
-
-	s, err := json.Marshal(items)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", "http://"+path.Join(url, "checkListings"), bytes.NewBuffer(s))
-	req.Header.Set("Content-type", "application/json")
-	if err != nil {
-		return nil, err
-	}
-	resp, err := c.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	decoder := json.NewDecoder(resp.Body)
-
-	if resp.StatusCode != 200 {
-		return nil, errors.New(fmt.Sprintf("Server returned %s", resp.Status))
-	}
-
-	var response []int
-	err = decoder.Decode(&response)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func crawlerMainLoop(maxParallelCoroutines int, chunkSize int, httpClassifierUrl string, crawler *crawling.Crawler) {
-	crawlerRound := 1
-	for {
-		startTime := time.Now()
-		processedCnt := 0
-		done := make(chan bool)
-		go func() {
-			lastNodeID := ""
-			for {
-				// get next chunk of nodes from database
-				nodesIDs, err := crawler.DB.GetNextNodesChan(lastNodeID, chunkSize)
-				if err != nil {
-					done <- true
-					return
-				}
-
-				atLeastOneNode := false
-				for nodeID := range nodesIDs {
-					atLeastOneNode = true
-
-					// create list of at max MAX_PARALLEL_COROUTINE nodes to start parallel coroutines
-					var lastNodes []string
-					lastNodes = append(lastNodes, nodeID)
-					nodesLen := maxParallelCoroutines - 1
-					for nodeID = range nodesIDs {
-						lastNodes = append(lastNodes, nodeID)
-						nodesLen--
-						if nodesLen <= 0 {
-							break
-						}
-					}
-					lastNodeID = nodeID
-
-					// start len(lastNodes) parallel coroutines
-					var wg sync.WaitGroup
-					wg.Add(len(lastNodes))
-					processedCnt += len(lastNodes)
-					processingStart := time.Now()
-					for i := range lastNodes {
-						go func(localNodeID string) {
-							defer wg.Done()
-							log.Debugf("Processing node with id: %s\n", localNodeID)
-							err := crawler.CrawlNode(localNodeID)
-							if err != nil {
-								log.Error(err)
-								return
-							}
-
-							log.Debugf("Crawling items for localNodeID: %s\n", localNodeID)
-							items, err := crawler.RPCInterface.GetItems(localNodeID)
-							if err != nil {
-								log.Error(err)
-								return
-							}
-
-							if httpClassifierUrl != "" && len(items) > 0 {
-								log.Debugf("Checking listings status using external service")
-								output, err := checkListings(httpClassifierUrl, items)
-								if err != nil {
-									log.Warning(err)
-								}
-								log.Debugf("Check finished with %s", output)
-
-								if len(output) == len(items) {
-									bannedCnt := 0
-									for index, blocked := range output {
-										if blocked != 0 {
-											bannedCnt += 1
-										}
-										items[index].Blocked = blocked > 0
-									}
-
-									if bannedCnt == 0 || float64(len(items))/float64(bannedCnt) < AllowThreshold { // automatically list owner
-										// automatically list stores with low level of banned listings
-										err := crawler.DB.UpdateNodeStatus(localNodeID, "listed", true)
-										if err != nil {
-											log.Warning("Cannot update node status listed = true for node %s", localNodeID)
-											log.Error(err)
-										} else {
-											log.Debugf("Updated node status listed = true for node %s", localNodeID)
-										}
-									} else if float64(len(items))/float64(bannedCnt) > BanThreshold {
-										// automatically ban also entire owner store
-										err := crawler.DB.UpdateNodeStatus(localNodeID, "blocked", true)
-										if err != nil {
-											log.Warning("Cannot update node status banned = true for node %s", localNodeID)
-											log.Error(err)
-										} else {
-											log.Debugf("Updated node status banned = true for node %s", localNodeID)
-										}
-									} else {
-										log.Debugf("Node %s banned listing threshold in range (%f < %f < %f) - cannot decide automatically",
-											localNodeID, AllowThreshold, float64(len(items))/float64(bannedCnt), BanThreshold)
-									}
-								}
-							}
-
-							err = crawler.DB.AddItemsForNode(localNodeID, items)
-							if err != nil {
-								log.Error(err)
-								return
-							}
-						}(lastNodes[i])
-					}
-					wg.Wait()
-					log.Debugf("Processing of %d nodes took %s.", len(lastNodes), time.Since(processingStart))
-				}
-
-				if !atLeastOneNode {
-					break
-				}
-			}
-			done <- true
-		}()
-
-		select {
-		case <-done:
-			crawlerRound++
-			log.Debugf("Crawler round %d took %s and processed %d items", crawlerRound, time.Since(startTime), processedCnt)
-			processedCnt = 0
-		}
-	}
-}
 
 func main() {
 	// configure logger output format
@@ -224,7 +55,8 @@ func main() {
 
 	r := rpc.NewRPC(*rpcURL)
 
-	crawler := &crawling.Crawler{RPCInterface: r, DB: d}
+	crawler := &crawling.Crawler{RPCInterface: r, DB: d, MaxCoroutineCnt: *maxParallelCorutine, ChunkSize: *chunkSize,
+		HttpClassifierUrl: *httpClassifierUrl}
 
 	config, err := r.GetConfig()
 	if err != nil {
@@ -247,5 +79,5 @@ func main() {
 		log.Panic(err)
 	}
 
-	crawlerMainLoop(*maxParallelCorutine, *chunkSize, *httpClassifierUrl, crawler)
+	crawler.MainLoop()
 }
