774a8299134ab4aa132557513f04a99b26ab280d
anchaj <anchaj@users.noreply.github.com>
2020-04-10 17:44:22 +0200

    Downgrade to mysql.5.7 for easier usage with password connection.

--- apiserver/start_db_in_docker.sh
+++ apiserver/start_db_in_docker.sh
@@ -14,21 +14,14 @@ if [[ ${ALREADY_RUNNING} = true ]]; then
     NEW_PASSWORD=`docker exec -it ${DOCKER_INSTANCE_NAME} sh -c "cat /root/mysql_pass.txt | tr -d '\n'"`
     echo "Container is already running with password ${NEW_PASSWORD}"
 else
-    docker pull mysql/mysql-server:8.0
+    docker pull mysql/mysql-server:5.7
     echo "Starting mysql with password ${NEW_PASSWORD}"
-    docker run --name=${DOCKER_INSTANCE_NAME} -d -p ${PORT}:3306 -e MYSQL_ROOT_PASSWORD=${NEW_PASSWORD} -e MYSQL_DATABASE=${DB_NAME} -e MYSQL_ROOT_HOST=% mysql/mysql-server:8.0
-    sleep 5
-    READY=''
-    while [[ -z ${READY} ]]; do
-        READY=`docker logs ${DOCKER_INSTANCE_NAME} 2>&1 | grep '/usr/sbin/mysqld: ready for connections'`
-        echo "Waiting for mysql to be ready"
-        sleep 2
-    done
-    echo "Server ready:   ${READY}"
     docker exec -it ${DOCKER_INSTANCE_NAME} sh -c "echo ${NEW_PASSWORD} > /root/mysql_pass.txt"
+    docker run --name=${DOCKER_INSTANCE_NAME} -d -p ${PORT}:3306 -e MYSQL_ROOT_PASSWORD=${NEW_PASSWORD} -e MYSQL_DATABASE=${DB_NAME} -e MYSQL_ROOT_HOST=% mysql/mysql-server:5.7
     echo "Started mysql instance on localhost:${PORT} with root user and '$NEW_PASSWORD'"
 fi
 
+echo ""
 echo "Now run npm init with DATABASE_URI set to: "
 echo "mysql://root:${NEW_PASSWORD}@127.0.0.1:${PORT}/${DB_NAME}"
 echo "OR connect to mysql from host"
